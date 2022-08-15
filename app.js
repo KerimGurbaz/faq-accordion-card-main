b5418a648f546e2a810ddc58453db99f9244c8a3
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-08-16 00:51:14 +0200

    js done

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var questions = document.getElementsByClassName("question");
+
+var i ;
+
+for(i=0; i<questions.length ; i++){
+    questions[i].addEventListener("click",function(){
+        console.log(this);
+        this.classList.toggle("active");
+
+        var panel = this.lastElementChild;
+        if(panel.style.display === "block"){
+            panel.style.display = "none";
+            panel.parentElement.firstElementChild.lastElementChild.style.transform = "rotate(180deg)"
+
+        } else{
+            panel.style.display = "block"
+        }
+
+    });
+}
